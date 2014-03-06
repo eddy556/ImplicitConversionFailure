8077ca2902359b6e36ebc6cac105d3124fb5d692
eddy556 <eddy556@gmail.com>
2014-03-06 23:35:31 +0000

    Initial Commit

---
+++ ImplicitConversionFailure/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitConversionFailure
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Precision(Decimal number)
+        {
+            var bits = Decimal.GetBits(number)[3];
+            var bytes = BitConverter.GetBytes(bits)[2];
+
+            return bytes;
+
+            //return BitConverter.GetBytes(Decimal.GetBits(number)[3])[2];
+        }
+
+        //broken (cast):
+        //[0] = 10
+        //[4] = 196608
+
+        //broken (convert):
+        //[0]
+        //[4]
+
+        //working:
+        //[0] = 1
+        //[4] = 131072
+
+        //
+    }
+}
---
+++ ImplicitConversionFailureTest/ProgramTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImplicitConversionFailure;
+
+namespace ImplicitConversionFailureTest
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        private decimal expectedDecimalPlaces = 2;
+
+        [TestMethod]
+        public void Test2DecimalPoint_WithDecimal_ExpectSuccess()
+        {
+            decimal i = 0.01m;
+            int actual = Program.Precision(i);
+            Assert.AreEqual(actual, expectedDecimalPlaces);
+        }
+
+        [TestMethod]
+        public void Test2DecimalPoint_WithFloat_ExpectSuccess()
+        {
+            float i = 0.01f;
+            int actual = Program.Precision((decimal)i);
+            Assert.AreEqual(actual, expectedDecimalPlaces);
+        }
+    }
+}
